5512ec97bcb09ed6027d9160fb7732427482af4c
ethan <wy0353@gmail.com>
2021-01-21 20:31:58 +0900

    Added Date parser (6 digit)

--- helpers/python/converter.py
+++ helpers/python/converter.py
@@ -18,4 +18,25 @@ def dict_to_json(dict):
 
 
 def xml_to_json(xml):
-    return dict_to_json(string_to_dict(xml))
+    return json.loads(dict_to_json(string_to_dict(xml)))
+
+
+def year_digit_two_to_four(year):
+    return f"19{year}" if int(year) > 21 else f"20{year}"
+
+
+def six_digit_date_to_dict(date):
+    if len(date) == 6:
+        try:
+            year = year_digit_two_to_four(date[:2])
+            month = date[2:4]
+            day = date[4:]
+            return {
+                "year": year,
+                "month": month,
+                "day": day,
+            }
+        except Exception as e:
+            raise Exception(e)
+    else:
+        return f"hey, date does not 6 digit, i got {len(date)} digit."
